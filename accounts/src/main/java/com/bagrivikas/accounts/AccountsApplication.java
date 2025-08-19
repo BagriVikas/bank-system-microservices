1db20cbfb06f98109fd892090a56a913ec12242c
bagrivikas1998 <bagrivikas1998@gmail.com>
2025-08-19 21:28:13 +0530

    finish implementation of account microservice

--- accounts/src/main/java/com/bagrivikas/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/bagrivikas/accounts/AccountsApplication.java
@@ -1,9 +1,36 @@
 package com.bagrivikas.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation by Vikas Bagri",
+				version = "v1",
+				contact = @Contact(
+						name = "Vikas Bagri",
+						email = "bagrivikas1998@gmail.com",
+						url = "https://www.linkedin.com/in/vikas-bagri-281b01237/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.linkedin.com/in/vikas-bagri-281b01237/"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation by Vikas Bagri",
+				url = "https://www.linkedin.com/in/vikas-bagri-281b01237/"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
