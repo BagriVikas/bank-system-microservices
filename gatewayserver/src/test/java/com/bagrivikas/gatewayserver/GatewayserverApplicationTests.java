3b3d425aad6ebe605c1117c1d3515655c48b02cd
vikas bagri <bagrivikas1998@gmail.com>
2025-11-07 00:14:11 +0530

    add gateway server to docker compose

---
+++ gatewayserver/src/test/java/com/bagrivikas/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagrivikas.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
