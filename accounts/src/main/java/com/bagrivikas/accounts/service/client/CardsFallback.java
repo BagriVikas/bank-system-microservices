e0e5dca0e5c44a83ce12061296451dfce0a82d3a
vikas bagri <bagrivikas1998@gmail.com>
2025-12-03 19:32:39 +0530

    implement retry resiliency pattern for gateway server

---
+++ accounts/src/main/java/com/bagrivikas/accounts/service/client/CardsFallback.java
@@ -0,0 +1,15 @@
+package com.bagrivikas.accounts.service.client;
+
+import com.bagrivikas.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+
+}
