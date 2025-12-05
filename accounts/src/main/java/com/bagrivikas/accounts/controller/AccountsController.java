1c94a90b56ab318fde43c3692ec303b9d3726cc6
vikas bagri <bagrivikas1998@gmail.com>
2025-12-05 20:50:59 +0530

    implement rate limiter for accounts microservice using resilience4j

--- accounts/src/main/java/com/bagrivikas/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/bagrivikas/accounts/controller/AccountsController.java
@@ -3,6 +3,7 @@ package com.bagrivikas.accounts.controller;
 import com.bagrivikas.accounts.constants.AccountsConstants;
 import com.bagrivikas.accounts.dto.*;
 import com.bagrivikas.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -231,11 +232,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name= "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 21");
     }
 
     @Operation(
